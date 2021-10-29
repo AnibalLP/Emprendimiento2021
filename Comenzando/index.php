622d2ebd149f7b8cea41a3d99606f86bc4f4a81b
Romeo779 <73049273+Romeo779@users.noreply.github.com>
2021-10-29 13:45:43 -0600

    foter
    Hice el footer

--- Comenzando/index.php
+++ Comenzando/index.php
@@ -68,7 +68,7 @@
 </div>
 
 <!--FOOTER O PIE DE PÁGINA-->
-<footer class="bg-dark text-white">
+<footer  style="background-color: #2e95c7">
   <div class="container">
     <nav class="row">
       <!--LOGO-->
@@ -91,19 +91,24 @@
       <!--redes sociales-->
       <ul class="col-3 list-unstyled"> <!--Esta clase quita las viñetas a los elementos li-->
         <li class="font-weight-bold text-uppercase">Quick links</li>
-        <li>
-          <!--Facebook-->
-          <a href="#" class="text-reset">Link 1</a>
-          <!--Twitter-->
-          <a href="#" class="text-reset">Link 1</a>
-          <!--Instagram-->
-          <a href="#" class="text-reset">Link 1</a>
-          <!--Youtube-->
-          <a href="#" class="text-reset">Link 1</a>
-        </li>
+          <li>
+            <!--Facebook-->
+            <a href="#" class="text-reset">Link 1</a>
+            <!--Twitter-->
+            <a href="#" class="text-reset">Link 1</a>
+            <!--Instagram-->
+            <a href="#" class="text-reset">Link 1</a>
+            <!--Youtube-->
+            <a href="#" class="text-reset">Link 1</a>
+          </li>
       </ul>
       <!--marca registrada-->
-      &copy;
+      <div class="container">
+          <div class="m-4 d-flex justify-content-center align-items-center">
+          <!--Simbolo copyrigth    Espacio entre lineas   Simbolo marca registrada-->
+          Copyright &copy;         &nbsp;&nbsp;           &reg; Todos los derechos reservados 2021
+          </div>
+        </div>        
     </nav>
   </div>
 </footer>
