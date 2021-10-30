6483e245e3dd2bb5bad11abf0644ce07badf3cdc
Anibal Portillo <48638366+AnibalLP@users.noreply.github.com>
2021-10-30 00:30:14 -0600

    tutoriales con boostrap
    hice que se ordenaran un poco los videos

--- Comenzando/welcome/tutoriales/form.php
+++ Comenzando/welcome/tutoriales/form.php
@@ -100,14 +100,23 @@ $(".custom-file-input").on("change", function() {
 
  </main>
 
-
+ <div class="container">
+    <div class="d-flex">
      <?php
-    $query = $conn->prepare("SELECT * FROM videos");
+    $query = $conn->prepare("SELECT * FROM videos ORDER BY nombre DESC");
     $query->execute();
-  $conn;
+    $data = $query->fetchAll();
+        foreach ($data as $row):
+            $idu=$row['id_user']; 
+            $nombre=$row['nombre'];
+            $ubicacion = $row['ubicacion'];
+            echo "<div class='row m-3'>";
+            echo "<video class='col' src='videos/".$ubicacion."' controls width='250px' height='250px'>";
+            echo "</div>";
+        endforeach;
         ?>    
- 
-
+ </div>
+</div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>    <!-- Bootstrap CSS -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
